7a33fac0e9b2e60235161f8eacb20d97d20af11e
Larissa Pereira <larissa.pereira@conecthus.org.br>
2023-02-08 10:15:35 -0400

    :wrench: feat: list entries and viewmodel

---
+++ app/src/main/java/com/laros/testpkdx/data/models/PokedexListEntry.kt
@@ -0,0 +1,8 @@
+package com.laros.testpkdx.data.models
+
+data class PokedexListEntry(
+    val pokemonName: String,
+    val imageUrl: String,
+    val number: Int
+
+)
--- app/src/main/java/com/laros/testpkdx/pokemonlist/PokeListScreen.kt
+++ app/src/main/java/com/laros/testpkdx/pokemonlist/PokeListScreen.kt
@@ -2,23 +2,37 @@ package com.laros.testpkdx.pokemonlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.laros.testpkdx.R
+import com.laros.testpkdx.data.models.PokedexListEntry
+import com.laros.testpkdx.ui.theme.RobotoCondensed
 
 @Composable
 fun PokeListScreen(
@@ -80,7 +94,7 @@ fun SearchBar(
                     isHintDisplayed = it.isFocused != true
                 }
         )
-        if(isHintDisplayed) {
+        if (isHintDisplayed) {
             Text(
                 text = hint,
                 color = Color.LightGray,
@@ -90,4 +104,97 @@ fun SearchBar(
         }
     }
 }
+
+@Composable
+fun PokeEntry(
+    entry: PokedexListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokeListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+    Box(
+        contentAlignment = Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "poke_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }
+    ) {
+        Column {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(entry.imageUrl)
+                    .crossfade(true)
+                    .target {
+                        viewModel.calcDominantColor(it) { color ->
+                            dominantColor = color
+                        }
+                    }
+                    .build(),
+                contentDescription = entry.pokemonName,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            )
+            Text(
+                text = entry.pokemonName,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun PokeRow(
+    rowIndex: Int,
+    entries: List<PokedexListEntry>,
+    navController: NavController
+
+) {
+    Column {
+        Row {
+            PokeEntry(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= rowIndex * 2 + 2) {
+                PokeEntry(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = (Modifier.weight(1f)))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+
+
+
+
+
 
